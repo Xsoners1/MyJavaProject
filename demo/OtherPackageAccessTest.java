154e15203bf1e728121292221383fc847b4f01e6
Xsoners1 <cvova527@gmail.com>
2025-12-22 02:43:53 +0200

    commit

--- demo/OtherPackageAccessTest.java
+++ demo/OtherPackageAccessTest.java
@@ -5,8 +5,19 @@ import app.people.Student;
 
 public class OtherPackageAccessTest {
     public static void main(String[] args) {
-        LiveOnlineCourse c = new LiveOnlineCourse(1, "Тест", "Опис", "Zoom", "link");
-        Student s = new Student(1, "Тест-студент", "bio");
+
+        LiveOnlineCourse c = new LiveOnlineCourse(
+                "Тест",
+                "Тест",
+                "Опис",
+                "link"
+        );
+
+        Student s = new Student(
+                "Тест",
+                "Студент",
+                "bio"
+        );
 
         System.out.println("Демонстрація package-private: доступ з іншого пакета заборонено.");
     }
